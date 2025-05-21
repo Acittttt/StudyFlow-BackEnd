d3a52d94635db575c474e2cc13c71fe4dbd763d5
Acittt <rasyiidraafi05@gmail.com>
2025-05-21 21:23:14 +0700

    fix: upload.single error in profileRoutes.js

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const profileController = require('../controllers/profileController');
 const authenticate = require('../middlewares/authMiddleware');
-const upload = require('../middlewares/uploadMiddleware');
+const { upload } = require('../middlewares/uploadMiddleware');
 
 // PUT profile/profile
 router.put('/edit', authenticate, upload.single('profile_picture'), profileController.updateProfile);
